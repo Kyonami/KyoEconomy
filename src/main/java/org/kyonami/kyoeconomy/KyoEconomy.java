685958f733e6118532f67ddf4734c460dfa0781b
Kyonami <tlaryska12@gmail.com>
2024-01-05 21:19:12 +0900

    first commit

---
+++ src/main/java/org/kyonami/kyoeconomy/KyoEconomy.java
@@ -0,0 +1,18 @@
+package org.kyonami.kyoeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KyoEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        // Check some
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
